ca11c3436d323009f043bde6ed5650725d8f7ec2
greenhornsr <srollins11279@gmail.com>
2020-04-21 14:14:04 -0700

    added bonus check for system argument to include a program file by checking for sys.argv length greater than 1.  confirmed working

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -9,7 +9,12 @@ cpu = CPU()
 
 # Program file from parsing CLI
 # argv_program_counter = 1  # for all file implementation
-program = sys.argv[1]
 
-cpu.load(program)
-cpu.run()
+# print(len(sys.argv))
+if len(sys.argv) > 1:
+    program = sys.argv[1]
+    
+    cpu.load(program)
+    cpu.run()
+else: 
+    print("Must pass a program argument to load() method.")
